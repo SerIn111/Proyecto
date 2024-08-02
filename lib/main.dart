a07a6b5ff2e1b520b686e6b6fe40d66f983ce951
SerIn111 <amayasergio31@gmail.com>
2024-08-02 08:19:54 -0600

    cambio de pagina principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_lenguajes/widget/customform.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class LogInPage extends StatefulWidget {
+  LogInPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LogInPage> createState() => _LogInPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _LogInPageState extends State<LogInPage> {
+  final usuarioController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final contraController = TextEditingController();
+  //el controlador del Form
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Center(
+          child: Text('Inicia Sesion')),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: formkey,
+          child: Column(
+            children: [
+              CustomFormInput(
+                controller: usuarioController,
+                label: 'Usuario',
+                hintText: 'Introduzca el usuario',
+                preicono: Icon(Icons.person),
+                validator: (valor){
+                  if (valor=='')
+                  {
+                    return 'El usuario no debe de estar vacio';
+                  }
+                  else if (valor!='sergio.inestroza@unah.hn')
+                  {
+                    return 'El usuario es incorrecto';
+                  }
+                },
+                texto: false,
+                maxx: 30,
+              ),
+              CustomFormInput(
+                controller: contraController,
+                label: 'Contraseña',
+                hintText: 'Introduzca la contraseña',
+                texto: true,
+                preicono: Icon(Icons.password),
+                suicono: Padding(
+                  padding: EdgeInsetsDirectional.only(end: 12.0),
+                  child: IconButton(
+                    onPressed: (){
+                      Icon(Icons.remove_red_eye);
+                      
+                      setState(() {
+                          
+                      });
+                    }, 
+                    icon: Icon(Icons.remove_red_eye_outlined),
+                  ),
+                ),
+                validator: (valor){
+                  if (valor == '')
+                  {
+                    return 'La contraseña no debe de estar vacia';
+                  }
+                  else if (valor!='20182002621')
+                    {
+                      return 'la contraseña es incorrecta';
+                      
+                    }
+                },
+                
+              ),
+              OutlinedButton(
+                child: Text(
+                  'Iniciar sesion', 
+                  style: TextStyle(
+                    fontSize: 20
+                    ),
+                  ),
+                onPressed: () {
+                  if (!formkey.currentState!.validate()) return;
+
+                  final datos = {
+                    'correo': usuarioController.text,
+                    'contrasenia': contraController.text,
+                  };
+
+                  print(datos);
+
+                },
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text('¿No tienes cuenta? Registrate'))
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      // floatingActionButton: FloatingActionButton(
+      //   child: Icon(Icons.save),
+      //   onPressed: () {
+      //     if (!formkey.currentState!.validate()) return;
+
+      //     final datos = {
+      //       'correo': correoController.text,
+      //       'contrasenia': contraseniaController.text,
+      //     };
+
+      //     print(datos);
+
+      //     // mandar a guardar
+      //   },
+      // ),
     );
   }
 }
---
+++ lib/widget/customform.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class CustomFormInput extends StatelessWidget {
+  CustomFormInput({
+    super.key,
+    required this.label,
+    required this.controller,
+    this.validator,
+    required this.hintText,
+    this.preicono,
+    this.suicono,
+    required this.texto,
+    this.maxx,
+  });
+
+  final String label;
+  final TextEditingController controller;
+  final String? Function(String?)? validator;
+  final String? hintText;
+  final Widget? preicono;
+  final Widget? suicono;
+  final bool texto;
+  final int? maxx;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      keyboardType: TextInputType.text,
+      maxLength: maxx,
+      maxLines: 1,
+      decoration: InputDecoration(
+        hintText: hintText,
+        label: Text(label),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+        // suffix: Icon(Icons.person),
+        errorText: null, //'El nombre es obligatorio',
+        // suffixIcon: Icon(Icons.remove_red_eye),
+        prefixIcon: preicono,
+        suffixIcon: suicono,
+        
+      ),
+      obscureText: texto,
+      // decoration: InputDecoration(
+      //   hintText: 'Ingrese su nombre',
+      //   label: Text(label),
+      //   border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+      //   // suffix: Icon(Icons.person),
+      //   errorText: null, //'El nombre es obligatorio',
+      //   // suffixIcon: Icon(Icons.remove_red_eye),
+      //   prefixIcon: Icon(Icons.person),
+        
+      // ),
+    );
+  }
+}
